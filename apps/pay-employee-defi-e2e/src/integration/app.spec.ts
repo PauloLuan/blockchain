9e06a04e11bd578918f8a3a85d995284a545e8a1
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-29 10:46:39 -0300

    📦 add: added initial monorepo structure

---
+++ apps/pay-employee-defi-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po'
+
+describe('pay-employee-defi', () => {
+  beforeEach(() => cy.visit('/'))
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword')
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome to pay-employee-defi!')
+  })
+})
