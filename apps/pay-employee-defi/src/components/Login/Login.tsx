3260357c991090877f7b6921cb68655a7edc5d87
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-30 07:55:12 -0300

    🎨 style: added some helpers to initial form

--- apps/pay-employee-defi/src/components/Login/Login.tsx
+++ apps/pay-employee-defi/src/components/Login/Login.tsx
@@ -1,24 +1,50 @@
-import { Flex, Stack, Input, Button } from '@chakra-ui/react'
+import {
+  Flex,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Button
+} from '@chakra-ui/react'
 
 export const Login = () => {
   return (
     <Flex v='100vh' h='100vh' align='center' justify='center'>
-      <Flex as='form'>
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        borderRadius={8}
+        borderColor='white'
+      >
         <Stack>
-          <Input
-            data-testid='login-email-field'
-            name='name'
-            type='email'
-          ></Input>
-          <Input
-            data-testid='login-password-field'
-            name='password'
-            type='password'
-          ></Input>
+          <FormControl size='lg'>
+            <FormLabel>Seu Email</FormLabel>
+            <Input
+              data-testid='login-email-field'
+              name='name'
+              type='email'
+              size='lg'
+            ></Input>
+            <FormHelperText>Pare de usar e-mail da bol</FormHelperText>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Sua senha:</FormLabel>
+            <Input
+              data-testid='login-password-field'
+              name='password'
+              type='password'
+              size='lg'
+            ></Input>
+            <FormHelperText>Respeitamos a LGPD</FormHelperText>
+          </FormControl>
+
           <Button
             data-testid='login-submit-button'
             colorScheme='blue'
             variant='solid'
+            size='lg'
             _hover={{
               bgColor: 'blue.900'
             }}
