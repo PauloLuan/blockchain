940b5952703fc84ed5ff552586d412082f511533
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-30 06:24:51 -0300

    📦 add: initial version of login screen

--- apps/pay-employee-defi/src/components/Login/Login.tsx
+++ apps/pay-employee-defi/src/components/Login/Login.tsx
@@ -1,9 +1,29 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Stack, Input, Button } from '@chakra-ui/react'
 
 export const Login = () => {
   return (
-    <Flex>
-      <h1 data-testid='login'>mindingo</h1>
+    <Flex
+      data-testid='login'
+      v='100vh'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex as='form'>
+        <Stack>
+          <Input name='name' type='email'></Input>
+          <Input name='password' type='password'></Input>
+          <Button
+            colorScheme='blue'
+            variant='solid'
+            _hover={{
+              bgColor: 'blue.900'
+            }}
+          >
+            Sign In
+          </Button>
+        </Stack>
+      </Flex>
     </Flex>
   )
 }
--- apps/pay-employee-defi/src/pages/index.tsx
+++ apps/pay-employee-defi/src/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Login } from '../components/Login'
 
 const StyledPage = styled.div`
   .page {
   }
 `
 
-export function Index() {
+export function Index () {
   return (
     <StyledPage>
-      <h1>Welcome</h1>
+      <Login />
     </StyledPage>
   )
 }
