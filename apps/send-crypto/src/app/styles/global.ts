76a0abdeef0004d0bd3a30619dd99db5076ad303
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-03 14:27:51 -0300

    🎨 style: added media to change font-size according to users display config

--- apps/send-crypto/src/app/styles/global.ts
+++ apps/send-crypto/src/app/styles/global.ts
@@ -6,6 +6,15 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  html {
+    @media (min-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (min-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background: var(--primary);
     -webkit-font-smoothing: antialiased;
@@ -17,5 +26,4 @@ export const GlobalStyles = createGlobalStyle`
   :root {
     --primary: #fff;
   }
-
 `
