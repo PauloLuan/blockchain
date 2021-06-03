156590993782b294e0a5c5868faf7983283167f9
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-03 04:26:17 -0300

    📦 add: added small adjustments for chakra generator

--- libs/generator/plopfile.js
+++ libs/generator/plopfile.js
@@ -40,4 +40,40 @@ module.exports = function (plop) {
       }
     ]
   })
+
+  plop.setGenerator('component:chakra', {
+    description: 'chakra: generates a new component with tests and storybook',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path:
+          './src/components/{{pascalCase name}}/{{kebabCase name}}.component.tsx',
+        templateFile: 'templates/chakra/component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: './src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/chakra/index.ts.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          './src/components/{{pascalCase name}}/{{kebabCase name}}.spec.tsx',
+        templateFile: 'templates/chakra/spec.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          './src/components/{{pascalCase name}}/{{kebabCase name}}.stories.tsx',
+        templateFile: 'templates/chakra/stories.tsx.hbs'
+      }
+    ]
+  })
 }
