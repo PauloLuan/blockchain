9df9cd2006f87bf4df745b6f99ea2d7763a65e76
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-03 14:39:26 -0300

    🎨 style: changed font-weight for some tags

--- apps/send-crypto/src/app/styles/global.ts
+++ apps/send-crypto/src/app/styles/global.ts
@@ -17,6 +17,15 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
   }
+
+  body, input, textarea, button {
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   html {
     @media (min-width: 1080px) {
       font-size: 93.75%;
